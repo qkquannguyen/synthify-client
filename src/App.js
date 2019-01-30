05215ab72f4b84de4f69cea80a77317d4bbd85cd
Dominic <domyoung424@gmail.com>
2019-01-30 14:56:50 -0600

    lint everything but pages/ and components/

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import LoginPage from './Pages/LoginPage';
-import SignUpPage from './Pages/SignupPage';
-import SettingsPage from './Pages/SettingsPage';
-import PrimarySearchAppBar from './Pages/HomePage';
-import { createMuiTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LoginPage from './Pages/LoginPage'
+import SignUpPage from './Pages/SignupPage'
+import SettingsPage from './Pages/SettingsPage'
+import PrimarySearchAppBar from './Pages/HomePage'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 
-import './App.css';
-const theme = createMuiTheme({ typography: { useNextVariants: true } });
+import './App.css'
+const theme = createMuiTheme({ typography: { useNextVariants: true } })
 class App extends Component {
   render() {
     return (
@@ -20,16 +20,19 @@ class App extends Component {
               component points to a react component
               Example https://reacttraining.com/react-router/web/example/basic
             */}
-            <Route exact path="/" component={LoginPage} /> {/*Located at src/Pages/LoginPage.js */}
-            <Route path="/home" component={PrimarySearchAppBar} /> {/* src/Pages/HomePage.js */}
-            <Route path="/settings" component={SettingsPage} /> {/* src/Pages/SettingsPage.js */}
-            <Route path="/signup" component={SignUpPage} /> {/* src/Pages/SignupPage.js */}
+            <Route exact path="/" component={LoginPage} />{' '}
+            {/*Located at src/Pages/LoginPage.js */}
+            <Route path="/home" component={PrimarySearchAppBar} />{' '}
+            {/* src/Pages/HomePage.js */}
+            <Route path="/settings" component={SettingsPage} />{' '}
+            {/* src/Pages/SettingsPage.js */}
+            <Route path="/signup" component={SignUpPage} />{' '}
+            {/* src/Pages/SignupPage.js */}
           </div>
         </Router>
       </ThemeProvider>
-
-    );
+    )
   }
 }
 
-export default App;
+export default App

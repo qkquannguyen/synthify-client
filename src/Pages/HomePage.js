d7d332e4fde2dc9020253d7aa2079dcc5baf3883
Mitchel Smith <mitchel.smith@yahoo.com>
2019-02-09 02:25:54 -0600

    Added changing playlists.

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -10,34 +10,77 @@ class HomePage extends Component {
     this.state = {
       play: false,
       currentTrack: {track: "", artist: ""},
+      currentPlaylistIndex: 0,
       // Set our current index to null, the user could click any where in the songs array
       currentIndex: null,
+      music: this.playlists[0].music,
       shuffle: false,
       repeat: false,
       repeatOne: false,
     };
   }
 
-  music = [
-    {track: "This Was a Home Once", artist:"Bad Suns"},
-    {track: "Oceans Away", artist: "A R I Z O N A"},
-    {track: "Fool for Love", artist:"Lord Huron"},
-    {track: "Feel It Boy", artist: "VHS Collection"},
-    {track: "Track 5", artist: "Artist 5"},
-    {track: "Track 6", artist: "Artist 6"},
-    {track: "Track 7", artist: "Artist 7"},
-    {track: "Track 8", artist: "Artist 8"},
-    {track: "Track 9", artist: "Artist 9"},
-    {track: "Track 10", artist: "Artist 10"},
+  playlists = [
+    {name: "Cool Music",
+     music: [
+     {track: "This Was a Home Once", artist:"Bad Suns"},
+     {track: "Oceans Away", artist: "A R I Z O N A"},
+     {track: "Fool for Love", artist:"Lord Huron"},
+     {track: "Feel It Boy", artist: "VHS Collection"},
+     {track: "Track 5", artist: "Artist 5"},
+     {track: "Track 6", artist: "Artist 6"},
+     {track: "Track 7", artist: "Artist 7"},
+     {track: "Track 8", artist: "Artist 8"},
+     {track: "Track 9", artist: "Artist 9"},
+     {track: "Track 10", artist: "Artist 10"},
+     ]
+    },
+    {name: "Cooler Music",
+     music: [
+     {track: "Different This Was a Home Once", artist:"Bad Suns"},
+     {track: "Different Oceans Away", artist: "A R I Z O N A"},
+     {track: "Different Fool for Love", artist:"Lord Huron"},
+     {track: "Different Feel It Boy", artist: "VHS Collection"},
+     {track: "Different Track 5", artist: "Artist 5"},
+     {track: "Different Track 6", artist: "Artist 6"},
+     {track: "Different Track 7", artist: "Artist 7"},
+     {track: "Different Track 8", artist: "Artist 8"},
+     {track: "Different Track 9", artist: "Artist 9"},
+     {track: "Different Track 10", artist: "Artist 10"},
+     ]
+    },
+    {name: "Coolest Music",
+     music: [
+     {track: "Differenter This Was a Home Once", artist:"Bad Suns"},
+     {track: "Differenter Oceans Away", artist: "A R I Z O N A"},
+     {track: "Differenter Fool for Love", artist:"Lord Huron"},
+     {track: "Differenter Feel It Boy", artist: "VHS Collection"},
+     {track: "Differenter Track 5", artist: "Artist 5"},
+     {track: "Differenter Track 6", artist: "Artist 6"},
+     {track: "Differenter Track 7", artist: "Artist 7"},
+     {track: "Differenter Track 8", artist: "Artist 8"},
+     {track: "Differenter Track 9", artist: "Artist 9"},
+     {track: "Differenter Track 10", artist: "Artist 10"},
+     ]
+    },
   ]
 
   shuffledMusic = []
 
   toggleAudio = () => { this.setState({ play: !this.state.play }); };
 
+  changePlaylist = (index) => { 
+    this.setState({ 
+      currentPlaylistIndex: index,
+      music: this.playlists[index].music,
+      // Current index -1 so when next is pressed, first song of new playlist will be played
+      currentIndex: -1,
+     });
+  };
+
   changeSong = (index, lastIndex) => {
     // Pressing next at end of song list
-    if (index >= this.music.length && this.state.repeat) {
+    if (index >= this.state.music.length && this.state.repeat) {
       index = 0;
     }
     // Pressing prev at beginning of song list
@@ -45,7 +88,7 @@ class HomePage extends Component {
       index = this.music.length - 1;
     }
     // Same as both before but if repeat is not on
-    else if ((index < 0 || index >= this.music.length) && !this.state.repeat) {
+    else if ((index < 0 || index >= this.state.music.length) && !this.state.repeat) {
       index = 0;
     }
 
@@ -57,7 +100,7 @@ class HomePage extends Component {
     // Shuffle on and song selected
     if (this.state.shuffle && lastIndex == null) {
       // Reset shuffled array then shuffle
-      this.shuffledMusic = Array.from(this.music);
+      this.shuffledMusic = Array.from(this.state.music);
       this.shuffledMusic = this.shuffleSongs(this.shuffledMusic, index);
       index = 0;
     }
@@ -65,7 +108,7 @@ class HomePage extends Component {
     // If shuffle off, use original array, else use shuffled array
     if (!this.state.shuffle) {
       // Set our current index state
-      this.setState({ currentTrack: this.music[index], currentIndex: index }); 
+      this.setState({ currentTrack: this.state.music[index], currentIndex: index }); 
     }
     else {
       this.setState({ currentTrack: this.shuffledMusic[index], currentIndex: index });
@@ -77,12 +120,12 @@ class HomePage extends Component {
 
     if (!this.state.shuffle) {
       // Reset shuffled music array then shuffle
-      this.shuffledMusic = Array.from(this.music);
+      this.shuffledMusic = Array.from(this.state.music);
       this.shuffledMusic = this.shuffleSongs(this.shuffledMusic, index);
     }
     else {
       // If shuffle is turned off, get index of song in original list
-      this.setState({ currentIndex: this.music.indexOf(this.shuffledMusic[index]) });
+      this.setState({ currentIndex: this.state.music.indexOf(this.shuffledMusic[index]) });
     }
   };
 
@@ -91,7 +134,7 @@ class HomePage extends Component {
 
     // Remove current song to replace in beginning after shuffled
     if (index != null) {
-      currentSong = this.music[index];
+      currentSong = this.state.music[index];
       this.shuffledMusic.splice(index, 1);
     }
 
@@ -126,8 +169,7 @@ class HomePage extends Component {
     }
     // Clicked again, turn both off
     else {
-      this.setState({ repeat: false });
-      this.setState({ repeatOne: false });
+      this.setState({ repeat: false, repeatOne: false });
     }
   };
 
@@ -136,18 +178,22 @@ class HomePage extends Component {
       <div>
         <AppBar />
         <PlaylistDrawer
+        // Functions
         toggleAudio={this.toggleAudio}
         changeSong={this.changeSong}
         shuffleState={this.shuffleState}
         repeatSongs={this.repeatSongs}
+        changePlaylist={this.changePlaylist}
+        // States and variables
         audioState={this.state.play}
-        music={this.music}
+        music={this.state.music}
+        currentPlaylistIndex={this.state.currentPlaylistIndex}
         currentTrack={this.state.currentTrack}
-        // Pass it down
         currentIndex={this.state.currentIndex}
         shuffle={this.state.shuffle}
         repeat={this.state.repeat}
         repeatOne={this.state.repeatOne}
+        playlists={this.playlists}
         />
       </div>
     );
--- src/components/PlaylistDrawer.js
+++ src/components/PlaylistDrawer.js
@@ -56,9 +56,9 @@ function PermanentDrawerLeft(props) {
         <Divider />
 				<h2> Playlists</h2>
         <List>
-          {['Playlist1', 'Playlist2', 'Playlist3',].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
+          {props.playlists.map((playlist, index) => (
+            <ListItem button key={index} onClick={() => props.changePlaylist(index)}>
+              <ListItemText primary={playlist.name} />
             </ListItem>
           ))}
         </List>
@@ -67,14 +67,14 @@ function PermanentDrawerLeft(props) {
       <main className={classes.content}>
         {/* <div className={classes.toolbar} /> */}
 					<Grid container direction='column' alignItems='stretch' className={classes.root}>
-						<h2>Playlist 1</h2>
+						<h2>{props.playlists[props.currentPlaylistIndex].name}</h2>
 						<List className={classes.list}>
                 {/* We are previously passing down an array of objects,
                 the first argument of map is a callback to the object in an array,
                 we need to access the property of that object, in this case its the "track"
                 property.
                 */}
-								{props.music.map((song, index) => (
+								{props.playlists[props.currentPlaylistIndex].music.map((song, index) => (
 								<ListItem button key={index} onClick={() => props.changeSong(index)}>
 								<ListItemText primary={song.track} />
 							</ListItem>

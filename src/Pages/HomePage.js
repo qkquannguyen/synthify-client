8e839585ba60fe1ba8cfdf45e616145c00a75f47
Quan Nguyen <qk.quannguyen@gmail.com>
2019-04-05 10:20:33 -0500

    add: ability to redirect to settings page upon clicking on the settings icon in home page (#1)
    * add: redirect state to go from home to settings
    
    * add: redirect state to go from home to settings
    
    * add: redirect to settings upon clicking settings icon
    
    * chore: refactor to make redirect to settings more clean
    
    * revert
    
    * chore: delete dummy music object

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import AppBar from '../components/AppBar'
 import PlaylistDrawer from '../components/PlaylistDrawer'
 
 class HomePage extends Component {
@@ -9,22 +11,20 @@ class HomePage extends Component {
       play: false,
       currentTrack: { track: '', artist: '' },
       // Set our current index to null, the user could click any where in the songs array
-      currentIndex: null
+      currentIndex: null,
+      redirect: false
     }
   }
 
-  music = [
-    { track: 'This Was a Home Once', artist: 'Bad Suns' },
-    { track: 'Oceans Away', artist: 'A R I Z O N A' },
-    { track: 'Fool for Love', artist: 'Lord Huron' },
-    { track: 'Feel It Boy', artist: 'VHS Collection' },
-    { track: 'Track 5', artist: 'Artist 5' },
-    { track: 'Track 6', artist: 'Artist 6' },
-    { track: 'Track 7', artist: 'Artist 7' },
-    { track: 'Track 8', artist: 'Artist 8' },
-    { track: 'Track 9', artist: 'Artist 9' },
-    { track: 'Track 10', artist: 'Artist 10' }
-  ]
+  setRedirect = () => {
+    this.setState({ redirect: true })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/settings" />
+    }
+  }
 
   toggleAudio = () => {
     this.setState({ play: !this.state.play })
@@ -43,15 +43,21 @@ class HomePage extends Component {
 
   render() {
     return (
-      <PlaylistDrawer
-        toggleAudio={this.toggleAudio}
-        changeSong={this.changeSong}
-        audioState={this.state.play}
-        music={this.music}
-        currentTrack={this.state.currentTrack}
-        // Pass it down
-        currentIndex={this.state.currentIndex}
-      />
+      <div>
+        <AppBar
+          setRedirect={this.setRedirect}
+          renderRedirect={this.renderRedirect}
+        />
+        <PlaylistDrawer
+          toggleAudio={this.toggleAudio}
+          changeSong={this.changeSong}
+          audioState={this.state.play}
+          music={this.music}
+          currentTrack={this.state.currentTrack}
+          // Pass it down
+          currentIndex={this.state.currentIndex}
+        />
+      </div>
     )
   }
 }
--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -76,7 +76,11 @@ function SearchAppBar(props) {
   const { classes } = props
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        setRedirect={props.setRedirect}
+        renderRedirect={props.renderRedirect}
+      >
         <Toolbar>
           <Typography
             className={classes.title}
@@ -88,30 +92,26 @@ function SearchAppBar(props) {
           </Typography>
           <div className={classes.grow} />
           <div className={classes.search}>
-            {props.auth && (
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-            )}
-            {props.auth && (
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-              />
-            )}
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+            />
           </div>
-          {props.auth && (
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Open drawer"
-            >
-              <SettingsIcon />
-            </IconButton>
-          )}
+          {props.renderRedirect()}
+          <IconButton
+            className={classes.IconButton}
+            color="inherit"
+            onClick={props.setRedirect}
+            aria-label="Open drawer"
+          >
+            <SettingsIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>

a14801880aa8d038b09993ad0d2f9dedf2b47719
maryehuerta <mary.huerta@mavs.uta.edu>
2019-04-30 23:59:02 -0500

    Oauth page looks..okay

--- src/Pages/OAuthPage.js
+++ src/Pages/OAuthPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 // TODO: Make this less ugly
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
@@ -18,10 +18,9 @@ const theme = createMuiTheme({
       text: {
         // Some CSS
         background: 'linear-gradient(45deg, #FE6B8B 20%, #FF8E53 90%)',
-        borderRadius: 3,
-        border: 1,
-        color: 'white',
-        padding: '0 30px'
+        borderRadius: 15,
+        color: 'white'
+        // padding: '0 30px',
       }
     }
   }
@@ -37,28 +36,87 @@ class OAuthPage extends Component {
     console.log(token)
     return (
       <MuiThemeProvider theme={theme}>
-        <Grid>
+        <Grid
+          container
+          direction="column"
+          justify="flex-end"
+          alignItems="center"
+          style={{ marginTop: '100px' }}
+        >
           <div>
             {token && (
-              <a
-                style={{ textDecoration: 'none', color: '#b3b3b3' }}
-                href={`http://localhost:9292/login/google?t=${token}`}
+              <Button
+                style={{
+                  width: '250px',
+                  height: '110px',
+                  background: '#cccccc',
+                  margin: '100px'
+                }}
               >
-                google
-              </a>
+                <img
+                  width="30px"
+                  height="30px"
+                  alt="GoOgLe"
+                  src={require('../images/googleIcon.png')}
+                />
+                <a
+                  style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    fontSize: '36px',
+                    paddingLeft: '10px'
+                  }}
+                  href={`http://localhost:9292/login/google?t=${token}`}
+                >
+                  google
+                </a>
+              </Button>
             )}
             {token && (
-              <a
-                style={{ textDecoration: 'none', color: '#b3b3b3' }}
-                href={`http://localhost:9292/login/spotify?t=${token}`}
+              <Button
+                style={{
+                  width: '250px',
+                  height: '110px',
+                  background: '#1DB954',
+                  margin: '100px'
+                }}
               >
-                spotify
-              </a>
+                <img
+                  width="30px"
+                  height="30px"
+                  alt="NoT GoOgLe"
+                  src={require('../images/spotifyIcon.png')}
+                />
+                <a
+                  style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    fontSize: '36px',
+                    paddingLeft: '10px'
+                  }}
+                  href={`http://localhost:9292/login/spotify?t=${token}`}
+                >
+                  spotify
+                </a>
+              </Button>
             )}
           </div>
-          <Link to="/home" style={{ textDecoration: 'none', color: '#b3b3b3' }}>
-            <p> Continue to Home </p>
-          </Link>
+          {/* Directs user to home */}
+          <Button
+            style={{
+              background: 'linear-gradient(45deg, #FE6B8B 20%, #FF8E53 90%)',
+              border: 1,
+              color: 'white',
+              padding: '0 30px',
+              borderRadius: 26,
+              width: '300px'
+              // margin:'100px'
+            }}
+          >
+            <Link to="/home" style={{ textDecoration: 'none', color: 'white' }}>
+              <p> Continue </p>
+            </Link>
+          </Button>
         </Grid>
       </MuiThemeProvider>
     )
